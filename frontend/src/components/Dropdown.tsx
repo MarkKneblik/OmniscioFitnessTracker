d882d64e98167d70474da6549038b7bd74f40905
Mark Kneblik <markkneblik@gmail.com>
2024-08-19 20:26:16 -0700

    fixed white block at top of screen when clicking jamburger

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,27 +1,49 @@
-import { fallDown as Menu } from 'react-burger-menu';
-import { Spin as Hamburger } from 'hamburger-react'
-import {useState } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Font Awesome component
-import { faDumbbell } from '@fortawesome/free-solid-svg-icons'; // Import the barbell (dumbbell) icon
-import '../styles/dropdown.css'
-
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons'; // Import the barbell icon
+import '../styles/dropdown.css';
+import Cheeseburger from './CheeseBurger';
 
 export default function Dropdown() {
-    
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
+  const toggle = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setIsOpen(!isOpen);
+  };
 
-        <div>
-            <Hamburger toggled={isOpen} toggle={setIsOpen} />
-            
-            <Menu width={'20%'} isOpen={isOpen} onStateChange={({ isOpen }) => setIsOpen(isOpen)} className='bm-overlay'>
-                <Link to="/MyProgram" className="menu-item">My Program <FontAwesomeIcon icon={faDumbbell} /></Link>
-                <Link to="/MyMeasurements" className="menu-item">My Measurements</Link>
-                <Link to="/Trends" className="menu-item">My Trends</Link>
-            </Menu>
-        </div>
+  return (
+    <div id="outer-container">
+      <Menu
+        width={'20%'}
+        isOpen={isOpen}
+        onStateChange={({ isOpen }) => setIsOpen(isOpen)}
+        pageWrapId={'page-wrap'}
+        outerContainerId={'outer-container'}
+        className='bm-overlay'
+      >
+        <Link to="/MyProgram" className="menu-item">
+          My Program <FontAwesomeIcon icon={faDumbbell} />
+        </Link>
+        <Link to="/MyMeasurements" className="menu-item">My Measurements</Link>
+        <Link to="/Trends" className="menu-item">My Trends</Link>
+        
+      </Menu>
 
-    );
+      <main id="page-wrap">
+
+        <Cheeseburger
+          color={'#303030'}
+          width={60}
+          height={60}
+          isToggled={isOpen}
+          onClick={toggle}
+        />
+
+      </main>
+
+    </div>
+  );
 }
