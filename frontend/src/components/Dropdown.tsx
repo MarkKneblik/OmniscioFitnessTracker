dc7a661514d82bdd0968abd6a97fe4f5a4de0e5c
Mark Kneblik <markkneblik@gmail.com>
2024-08-20 07:28:57 -0700

    fully functioning drawer before altering the size of the exit button

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -4,7 +4,7 @@ import 'react-modern-drawer/dist/index.css'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Font Awesome component
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons'; // Import the barbell icon
-import '../styles/dropdown.css';
+import '../styles/drawer.css';
 import Cheeseburger from './CheeseBurger';
 import NavBar from '../pages/NavBar';
 
@@ -17,18 +17,28 @@ export default function Dropdown() {
 
   return (
     <div>
+      <div style={{
+        position: 'fixed',  // Keep icon fixed
+        zIndex: 1000        // Higher z-index to appear over the drawer
+      }}>
       <Cheeseburger
       color={'#303030'}
       width={40}
       height={40}
       isToggled={isOpen}
       onClick={toggleDrawer}
+      
       />
+      </div>
 
       <Drawer
           open={isOpen}
           onClose={toggleDrawer}
           direction='left'
+          style={{
+            width: '21%',
+            zIndex:999
+          }}
       >
         <NavBar></NavBar>
 
