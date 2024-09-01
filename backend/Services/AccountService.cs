754002ebdfc323a10c827baa43d67412820a5815
Mark Kneblik <markkneblik@gmail.com>
2024-09-01 07:52:36 -0700

    commenting for clarity

--- backend/Services/AccountService.cs
+++ backend/Services/AccountService.cs
@@ -15,16 +15,24 @@ public class AccountService
     public async Task FindOrCreateAccount() 
     {
         var httpContext = _httpContextAccessor.HttpContext;
+
+        // Represent the user
         var user = httpContext?.User;
+
+        // Create Claims Identity
         var identity = user?.Identity as ClaimsIdentity;
+
+        // Extract user's email and name from Claims Identity
         var userEmail = identity?.FindFirst(ClaimTypes.Email)?.Value;
         var userFirstName = identity?.FindFirst(ClaimTypes.GivenName)?.Value;
         var userLastName = identity?.FindFirst(ClaimTypes.Surname)?.Value;
         
-        if (userEmail != null)
+        if (userEmail != null) // If an email was extracted from Claims Identity
         {
+            // Attempt to find an account tied to the user making the request
             var account =  await _dbContext.Accounts.FirstOrDefaultAsync(account => account.Email == userEmail);
 
+            // If no account was found related to this user
             if (account == null)
             {
                 var newAccount = new AccountModel
